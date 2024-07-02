92961fdb23d3e32ef2cc11b6d9d53a7bb08708c5
Aakash9399 <akashpalliwar79@gmail.com>
2024-07-02 14:15:21 +0530

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+import Footer from './Footer';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='w-16 h-16 object-contain'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div>
+
+    <div className='bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about'>
+      <div className='flex flex-col justify-around'>
+        <div className='sm:px-16 px-2'>
+          <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
+          <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hello! I'm Aakash Palliwar, a soon-to-be 2025 graduate with a deep passion for software development.I've honed my skills in Python and SQL, building a strong foundation in creating efficient and scalable software solutions.🚀. 
+          <br/>
+          My journey in software engineering has been driven by a relentless curiosity and a desire to solve complex problems through innovative coding. Whether it's developing robust back-end systems or crafting seamless user experiences, I am dedicated to delivering high-quality software that makes a difference.</p>
+
+          <ButtonLink
+            url='https://docs.google.com/document/d/1r6J5CiSO4rtth4FjHp5-qrpr8WuUcm8r8F8sOpsrIok/edit'
+            text='View Resume →'
+            padding={`p-3`}
+          />
+
+        </div>
+        <div className='mt-20 flex justify-center flex-wrap gap-7'>
+          {services.map((service) => (
+            <ServiceCard service={service} />
+          ))}
+        </div>
+      </div>
+    </div>
+    <Footer/>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,50 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Python and SQL expert",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Aakash';
+
+export const experiences = [
+    {
+        'company': 'LeapAI',
+        'role': 'Data analytics Intern',
+        'duration': 'July 2023 - August 2023',
+        'logo': 'https://media.licdn.com/dms/image/D4D0BAQEnikCSyFZ_eg/company-logo_200_200/0/1687464152717/unleashleapai_logo?e=1727913600&v=beta&t=gi0oG7MYMj6lHr5k8ZvJSuyWNVpn1LwhSaHU97clr0M',
+        'points': [
+            'Involved in dynamic contributions to database and development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': 'https://www.linkedin.com/company/unleashleapai/about/',
+    },
+    {
+        'company': 'InternPe',
+        'role': 'Technical Intern',
+        'duration': 'June 2023 - july 2023',
+        'logo': 'https://media.licdn.com/dms/image/C4E0BAQGs7EmZ0yQMpQ/company-logo_200_200/0/1661493099915?e=1727913600&v=beta&t=hEip4jpFRqN_FqC4WXl8YTDBjceWBpS7sUmakzvlmag',
+        'points': [
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+        ],
+        'url': 'https://www.linkedin.com/company/internpe/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
