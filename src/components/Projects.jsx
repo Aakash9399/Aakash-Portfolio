ec2ee54e44021b641801ef75f42197001b1c30c9
Aakash9399 <akashpalliwar79@gmail.com>
2024-07-02 15:25:20 +0530

    add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,14 +7,14 @@ import Footer from './Footer'
 const ProjectCard = ({ image, title, description, git, technologies }) => {
     return (
         <div className="max-w-sm sm:max-w-sm md:max-w-sm bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            {title==='Snap Shot' && <a href="#">
+            {title==='Snap Shot' && <a href="#" target="_blank" rel="noreferrer">
                 <img className="w-full rounded-t-lg h-auto object-cover " src={vpn} alt="" />
             </a>}
-            {title==='Co People' && <a href="#">
+            {title==='Co People' && <a href="#" target="_blank" rel="noreferrer">
                 <img className="w-full rounded-t-lg h-auto object-cover " src={copeople} alt="" />
             </a>}
             <div className="p-4 sm:p-6">
-                <a href="#">
+                <a href="#" target="_blank" rel="noreferrer">
                     <h5 className="text-2xl sm:text-xl md:text-2xl lg:text-3xl font-bold tracking-tight text-white bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-pink-500">{title}</h5>
                 </a>
                 <p className="font-normal text-sm sm:text-base md:text-lg text-gray-300 dark:text-gray-400">{description}</p>
@@ -30,7 +30,7 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
                         </p>
                     ))}
                 </div>
-                <a href={git} className="text-red-300 border border-gray-200 rounded-lg shadow p-1 sm:p-2 lg:p-3 hover:text-green-500 duration-300">GitHub</a>
+                <a href={git} target="_blank" rel="noreferrer" className="text-red-300 border border-gray-200 rounded-lg shadow p-1 sm:p-2 lg:p-3 hover:text-green-500 duration-300">GitHub</a>
             </div>
         </div>
     );
